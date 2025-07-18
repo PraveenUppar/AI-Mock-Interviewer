d8928e6b3519e84ab6c7f7c6120ef3ce98c798f8
Praveen Uppar <praveenuppar718@gmail.com>
2025-07-18 22:48:48 +0530

    define routes and route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,26 @@
 import './App.css'
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import Toaster from 'react-hot-toast'
+
+import Login from "./pages/auth/login"
+import Signup from "./pages/auth/signup"
+import Landing from "./pages/landing"
+import Dashboard from "./pages/home/dashboard"
+import Interview from "./pages/interview/interview"
 
 function App() {
   return (
-    <>
-      <h1 className='text-5xl font-semibold'>Hello World</h1>
-    </>
+    <div>
+      <Router>
+      <Routes>
+        <Route path='/' element = {<Landing />}></Route>
+        <Route path='/login' element = {<Login />}></Route>
+        <Route path='/signup' element = {<Signup />}></Route>
+        <Route path='/dashboard' element = {<Dashboard />}></Route>
+        <Route path='/interview/:sessionId' element = {<Interview />}></Route>
+      </Routes>
+    </Router>
+    </div>
   )
 }
 
